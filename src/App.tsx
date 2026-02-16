70767b5f697b44d2108593ed6bdd7d91eac55479
Rizki Cahya <rizki.cahya03@gmail.com>
2026-02-16 16:30:46 +0700

    feat: add promo section

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import GameSection from "./components/GameSection";
-// import PromoSection from "./components/PromoSection";
+import PromoSection from "./components/PromoSection";
 import Footer from "./components/Footer";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
 
             <main className="max-w-7xl mx-auto px-4 sm:px-6 py-8">
                 <GameSection />
-                {/* <PromoSection /> */}
+                <PromoSection />
             </main>
 
             <Footer />
